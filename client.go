ae4af68bc3e4cc85ddd60fff62ba81233e84636d
Rexarrior <rexarrior@yandex.ru>
2020-09-09 19:52:37 +0300

    try to change intervals

--- client.go
+++ client.go
@@ -20,8 +20,8 @@ const (
 	clientReceiverReportInterval = 10 * time.Second
 	clientTCPReadBufferSize      = 128 * 1024
 	clientTCPWriteBufferSize     = 128 * 1024
-	clientUDPReadBufferSize      = 512
-	clientUDPWriteBufferSize     = 128 * 1024
+	clientUDPReadBufferSize      = 8096
+	clientUDPWriteBufferSize     = 512
 )
 
 type clientDescribeReq struct {
